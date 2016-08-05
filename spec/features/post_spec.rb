7fb8deb392e2480b8354709325165c5be4d4c746
Xiaofei <xifzhang@thoughtworks.com>
2016-08-05 14:54:04 +0800

    add ft for creating new item

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,18 +4,21 @@ RSpec.feature 'post feature', type: :feature do
   scenario 'create new post' do
     visit '/posts'
     sleep(1)
-    # expect(page).not_to have_content('initialize')
-    # expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
     click_link('New Post')
     fill_in('post_title', with: 'my first post')
-    sleep(1)
-    fill_in('post_content', with: 'I wanna be the full-stack dev')
-    sleep(1)
+    fill_in('post_content', with: 'my first content')
     click_button('Create Post')
     sleep(1)
-    visit '/posts'
+    expect(page).to have_content('Post was successfully created')
+
+    visit '/'
     sleep(1)
     expect(page).to have_content('my first post')
+    expect(page).to have_content('my first content')
+    expect(page).to have_content('TOP 10 Posts')
+    expect(page).to have_content('initialize component')
+    expect(page).to have_content('start loading...')
+    expect(page).to have_selector('ul li')
+    expect(find('ul li')).to have_content('my first post')
   end
 end
