0abf5a0eb6e7a00074f9cfa89d434bff20286670
Xiaofei <xifzhang@thoughtworks.com>
2016-08-17 19:37:20 +0800

    [xf] abstract new post page from main workflow

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,18 +5,20 @@ RSpec.feature 'post feature', type: :feature do
   scenario 'create new post' do
     visit '/posts'
     sleep(5)
-    on_page_with :post_list do |page|
-        expect(page.posts_in_list.length).to eq(0)
+    on_page_with :post_list do |post_list_page|
+        expect(post_list_page.posts_in_list.length).to eq(0)
         sleep(1)
-        page.new_post_link.click
+        post_list_page.new_post_link.click
         sleep(1)
-        page.perform :fill_post, 'my first post', 'I wanna be the full-stack dev'
+        on_page_with :new_post do |new_post_page|
+            new_post_page.perform :fill_post, 'my first post', 'I wanna be the full-stack dev'
+            sleep(1)
+            new_post_page.create_post_button.click
+        end
         sleep(1)
-        page.create_post_button.click
-        sleep(1)
-        expect(page.notice_message).to eq('Post was successfully created.')
-        page.back_link.click
-        expect(page.posts_in_list.length).to eq(1)
+        expect(post_list_page.notice_message).to eq('Post was successfully created.')
+        post_list_page.back_link.click
+        expect(post_list_page.posts_in_list.length).to eq(1)
     end
   end
 end
